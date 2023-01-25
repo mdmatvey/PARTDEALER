eaa9c88133fda01c07b0f1533160d3434555e134
mdmatvey <lyoshinmdmatvey@gmail.com>
2023-01-25 21:31:40 +0300

    feat: Add cart and ordering pages functionality

--- online-store/src/pages/Cart.js
+++ online-store/src/pages/Cart.js
@@ -1,15 +1,16 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect, useState } from "react";
 import { Button, Card, Container, Row } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Context } from "..";
 import CartList from "../components/CartList";
 import Total from "../components/Total";
-import { ORDERING_ROUTE } from "../utils/routeConsts";
+import { SHOP_ROUTE, ORDERING_ROUTE } from "../utils/routeConsts";
 import { setEnding } from "../utils/functions"
+import { PRIMARY_COLOR } from "../utils/uiConsts";
 
 const Cart = observer(() => {
-    const {cart} = useContext(Context);
+    const {product, cart} = useContext(Context);
     const navigate = useNavigate();
 
     const [itemsCount, setItemsCount] = useState(cart.cartItems.length);
@@ -27,50 +28,87 @@ const Cart = observer(() => {
                     <h1 style={{margin: 0}}>Корзина</h1>
                     <span style={{fontSize: 24, marginLeft: 20}}>{itemsCount} товар{ending}</span>
                 </div>
-                <span>
-                    <Button 
-                        onClick={() => {
-                            if (cart.selectedItems.length === cart.cartItems.length) {
-                                cart.setSelectedItems([]);
-                                for (let i = 0; i < cart.cartItems.length; i++) {
-                                    document.getElementsByClassName("cartChecked")[i].children[0].checked = false;
-                                }
-                            } else {
-                                cart.setSelectedItems(cart.cartItems)
-                                for (let i = 0; i < cart.cartItems.length; i++) {
-                                    document.getElementsByClassName("cartChecked")[i].children[0].checked = true;
-                                }
-                            }
-                        }}                        
-                        style={{color: "#000", background: "none", border: "none", borderRadius: "0"}}
-                    >
-                        Выбрать все
-                    </Button>
-                    <Button 
-                        onClick={() => {
-                            const selectedItemIds = cart.selectedItems.map(selectedItem => selectedItem.id) ? cart.selectedItems.map(selectedItem => selectedItem.id) : [];
-                            cart.setCartItems(cart.cartItems.filter(cartItem => !selectedItemIds.includes(cartItem.id)).map(item => item));
-                            cart.setSelectedItems([]);
-                            for (let i = 0; i < cart.cartItems.length; i++) {
-                                document.getElementsByClassName("cartChecked")[i].children[0].checked = false;
-                            }
-                        }}
-                        style={{color: "#000", background: "none", border: "none", borderRadius: "0"}}
-                    >
-                        Х Удалить ({cart.selectedItems.length})
-                    </Button>
-                </span>
             </Row>
-            <div style={{display: 'grid', gridTemplateColumns: '3fr 1fr'}}>
-                <CartList cartPage={true} />
-                <Card>
-                    <Button disabled={cart.cartItems.length > 0 ? false : true} onClick={() => navigate(ORDERING_ROUTE)}>Перейти к оформлению</Button>
-                    <Total itemsCount={itemsCount} />
-                </Card>
-            </div>
-            <Row>
-                <h2>Часто покупают с товарами из вашей корзины</h2>
-            </Row>
+                {
+                    itemsCount === 0 
+                    ?
+                        <div 
+                            className="d-flex flex-column align-items-center mt-5" 
+                            style={{display: "block", paddingTop: "5%", textAlign: "center"}}
+                        >
+                            <h3 style={{fontSize: "4rem"}}>В вашей корзине пока ничего нет</h3>
+                            <span 
+                                className="mt-3" 
+                                style={{fontSize: "1.5rem"}}
+                            >
+                                Посмотрите товары в нашем каталоге или воспользуйтесь поиском
+                            </span>
+                            <div className="d-inline-flex justify-content-between align-items-baseline w-25 mt-5">
+                                <Button 
+                                    onClick={() => {
+                                        product.setCategoriesToDisplay([]);
+                                        navigate(SHOP_ROUTE);
+                                    }}
+                                    style={{border: "none", borderRadius: 0, background: PRIMARY_COLOR, padding: "10px 20px", fontSize: "1.25rem", fontWeight: "bold"}}
+                                >
+                                    В каталог
+                                </Button>
+                                <Link to="/" style={{fontSize: "1.25rem", fontWeight: "bold", textDecoration: "none"}}>На главную</Link>
+                            </div>
+                        </div>
+                    :
+                    <>
+                        <span>
+                            <Button 
+                                onClick={() => {
+                                    if (cart.selectedItems.length === cart.cartItems.length) {
+                                        cart.setSelectedItems([]);
+                                        for (let i = 0; i < cart.cartItems.length; i++) {
+                                            document.getElementsByClassName("cartChecked")[i].children[0].checked = false;
+                                        }
+                                    } else {
+                                        cart.setSelectedItems(cart.cartItems)
+                                        for (let i = 0; i < cart.cartItems.length; i++) {
+                                            document.getElementsByClassName("cartChecked")[i].children[0].checked = true;
+                                        }
+                                    }
+                                }}                        
+                                style={{color: "#000", background: "none", border: "none", borderRadius: "0"}}
+                            >
+                                Выбрать все
+                            </Button>
+                            <Button 
+                                onClick={() => {
+                                    const selectedItemIds = cart.selectedItems.map(selectedItem => selectedItem.id) ? cart.selectedItems.map(selectedItem => selectedItem.id) : [];
+                                    cart.setCartItems(cart.cartItems.filter(cartItem => !selectedItemIds.includes(cartItem.id)).map(item => item));
+                                    cart.setSelectedItems([]);
+                                    for (let i = 0; i < cart.cartItems.length; i++) {
+                                        document.getElementsByClassName("cartChecked")[i].children[0].checked = false;
+                                    }
+                                }}
+                                style={{color: "#000", background: "none", border: "none", borderRadius: "0"}}
+                            >
+                                Х Удалить ({cart.selectedItems.length})
+                            </Button>
+                        </span>
+                        <div style={{display: 'grid', gridTemplateColumns: '3fr 1fr'}}>
+                            <CartList cartPage={true} />
+                            <Card
+                                style={{width: "100%", border: "none", borderRadius: 0, padding: 10}}
+                            >
+                                <Button 
+                                    disabled={cart.cartItems.length > 0 ? false : true} 
+                                    onClick={() => navigate(ORDERING_ROUTE)} 
+                                    className="mb-4"
+                                    style={{fontWeight: "bold", fontSize: "1.2rem", padding: 20, border: "none", borderRadius: 0, background: PRIMARY_COLOR}}
+                                >
+                                    Перейти к оформлению
+                                </Button>
+                                <Total itemsCount={itemsCount} />
+                            </Card>
+                        </div>
+                    </>
+                }
         </Container>
     );
 });
--- online-store/src/pages/Ordering.js
+++ online-store/src/pages/Ordering.js
@@ -1,14 +1,19 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext, useEffect, useState } from "react";
-import { Button, Card, Container, Row, Form, Image } from "react-bootstrap";
+import { Button, Card, Container, Modal, Row, Tab, Tabs, Form, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { Context } from "..";
 import Total from "../components/Total";
 import { CART_ROUTE } from "../utils/routeConsts";
+import { PRIMARY_COLOR } from "../utils/uiConsts";
 
 const Cart = observer(() => {
-    const {cart} = useContext(Context);
+    const {cart, user} = useContext(Context);
     const navigate = useNavigate(); 
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const [itemsCount, setItemsCount] = useState(cart.cartItems.length);
 
@@ -16,59 +21,127 @@ const Cart = observer(() => {
         setItemsCount(cart.cartItems.length);
     }, [cart.cartItems]);
 
+    const [productColumns, setProductColumns] = useState(8);
+    const [pageColumns, setPageColumns] = useState("3fr 1fr")
+
+    useEffect(() => {
+        if (user.userWidth < 500) {
+            setProductColumns(3);
+        } else if (user.userWidth < 768) {
+            setProductColumns(4);
+        } else if (user.userWidth < 992) {
+            setProductColumns(4);
+        } else if (user.userWidth < 1200) {
+            setProductColumns(6);
+        } else if (user.userWidth < 1400) {
+            setProductColumns(7);
+        } else if (user.userWidth >= 1400) {
+            setProductColumns(8);
+        }
+    }, [user.userWidth]);
+
+    useEffect(() => {
+        if (user.userWidth < 768) {
+            setPageColumns("1fr");
+        } else if (user.userWidth >= 768) {
+            setPageColumns("3fr 1fr");
+        }
+    }, [user.userWidth]);
+
     return (
         <Container className="mt-3">
-            <Row>
+            <Row className="mb-4">
                 <div className="d-flex flex-direction-row align-items-end">
                     <h1 style={{margin: 0}}>Оформление</h1>
                     <span onClick={() => navigate(CART_ROUTE)} style={{fontSize: 24, marginLeft: 20, cursor: "pointer"}}>Вернуться в корзину</span>
                 </div>
             </Row>
-            <div style={{display: 'grid', gridTemplateColumns: '3fr 1fr'}}>
+            <div style={{display: 'grid', gridTemplateColumns: `${pageColumns}`}}>
                 <Container>
-                    <Card>
+                    <Card className="p-3 mb-3" style={{borderRadius: 0}}>
                         <h2>Товары к оплате</h2>
-                        <Container className="d-flex">
-                            {cart.cartItems.map(cartItem => <><Image style={{objectFit: 'contain'}} width={50} height={50} src={cartItem.image} /> x {cartItem.count}</>)}
+                        <Container style={{display: "grid", gridTemplateColumns: `repeat(${productColumns}, 1fr)`, gap: 30}}>
+                            {
+                                cart.cartItems.map(cartItem => 
+                                    <div style={{position: "relative"}}>
+                                        <Image style={{objectFit: 'contain'}} width={75} height={75} src={cartItem.image} />
+                                        <div style={{position: "absolute", zIndex: 2, top: 0, right: 0, backdropFilter: "blur(10px)", background: "rgba(0, 0, 0, 0.3)", color: "#fff", padding: "2px 8px", borderRadius: 7}}>{cartItem.count}</div>
+                                    </div>
+                                )
+                            }
                         </Container>
                     </Card>
-                    <Card>
-                        <h2>Способ получения заказа</h2>
+                    <Card className="p-3 mb-3" style={{borderRadius: 0}}>
+                        <h2>Адрес </h2>
                         <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-    
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Введите адрес</Form.Label>
+                                <Form.Control type="text" placeholder="Город, улица, дом, квартира..." />
+                                <Form.Text className="text-muted">
+                                    Мы не разглашаем данные наших клиентов.
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Согласен с условиями доставки" />
+                            </Form.Group>
                         </Form>
                     </Card>
                     <div className="d-flex">
-                        <Card style={{width: "50%"}}>
+                        <Card className="p-3 me-2" style={{width: "50%", borderRadius: 0}}>
                             <h2>Получатель</h2>
-                            Вася пупкин
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Введите имя</Form.Label>
+                                    <Form.Control type="email" placeholder="Имя" />
+                                    <Form.Text className="text-muted">
+                                        Мы не разглашаем данные наших клиентов.
+                                    </Form.Text>
+                                </Form.Group>
+                            </Form>
                         </Card>
-                        <Card style={{width: "50%"}}>
+                        <Card className="p-3 ms-2" style={{width: "50%", borderRadius: 0}}>
                             <h2>Способ оплаты</h2>
-                            Карта
+                            <Tabs
+                                defaultActiveKey="profile"
+                                className="mb-3"
+                            >
+                                <Tab eventKey="home" title="Переводом">
+                                    <p>1234 5678 1234 5678 - Сбербанк</p>
+                                    <p>8 910 123 456 78 91 - Сбербанк</p>
+                                </Tab>
+                                <Tab eventKey="profile" title="СБП">
+                                    <Button onClick={handleShow} style={{border: "none", borderRadius: 0, background: PRIMARY_COLOR}}>
+                                        Нажмите для получения QR для оплаты
+                                    </Button>
+                                    <Modal show={show} onHide={handleClose} centered>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>QR код СБП</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <Image src="https://sbp.nspk.ru/api/img/QR_with_logo.png" style={{display: "block", margin: "0 auto"}}></Image>
+                                        </Modal.Body>
+                                    </Modal>
+                                </Tab>
+                                <Tab eventKey="contact" title="При получении">
+                                    <Form>
+                                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                            <Form.Check type="checkbox" label="Оплатить при получении" />
+                                        </Form.Group>
+                                    </Form>
+                                </Tab>
+                            </Tabs>
                         </Card>   
                     </div>
                 </Container>
-                <Card>
+                <Card style={{border: "none", width: "100%"}}>
                     <Total itemsCount={itemsCount} />
-                    <Button onClick={() => navigate()}>Оформить заказ</Button>
+                    <Button 
+                        onClick={() => navigate()}
+                        className="mt-4"
+                        style={{fontWeight: "bold", fontSize: "1.2rem", padding: 20, border: "none", borderRadius: 0, background: PRIMARY_COLOR}}
+                    >
+                        Оформить заказ
+                    </Button>
                 </Card>
             </div>
         </Container>
