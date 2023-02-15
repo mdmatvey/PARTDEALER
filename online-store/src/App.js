6ebb2e11fc58856029dd202bf28d04cba32f3b5b
mdmatvey <lyoshinmdmatvey@gmail.com>
2023-02-15 19:09:22 +0300

    feat: Add scrollbar styles

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/Footer'
 import { check } from './components/http/userAPI'
 import NavBar from './components/NavBar'
 import WebFont from 'webfontloader'
+import ScrollbarStyles from './styles/ScrollbarStyles.css'
 
 const App = observer(() => {
   const { user } = useContext(Context)
