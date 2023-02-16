29aa96ea291420ac6722622966717c453425202f
mdmatvey <lyoshinmdmatvey@gmail.com>
2023-02-16 13:11:52 +0300

    fix: Add disabled attribute if user unauthorized

--- online-store/src/components/CartButton.js
+++ online-store/src/components/CartButton.js
@@ -2,23 +2,37 @@ import React, { useContext } from 'react'
 import { Context } from '../index'
 import { TbShoppingCartPlus } from 'react-icons/tb'
 import EventStyles from '../styles/EventStyles.css'
+import { useNavigate } from 'react-router-dom'
+import { LOGIN_ROUTE } from '../utils/routeConsts'
 
 const Product = ({ item, productPage }) => {
-  const { cart } = useContext(Context)
+  const navigate = useNavigate()
+  const { user, cart } = useContext(Context)
 
   return (
-
-        <button
-            onClick={() => {
-              if (!cart.cartItems.map(item => item.id).includes(item.id)) {
-                cart.setCartItems([...cart.cartItems, item])
-              }
-            }}
-            className={`d-flex align-items-center justify-content-center main-button ${productPage ? 'inverted' : null}`}
-            style={{ height: productPage ? '100%' : null }}
-        >
-              В корзину&nbsp;<TbShoppingCartPlus />
-        </button>
+    user.isAuth
+      ? <button
+        onClick={() => {
+          if (!cart.cartItems.map(item => item.id).includes(item.id)) {
+            cart.setCartItems([...cart.cartItems, item])
+          }
+        }}
+        className={`d-flex align-items-center justify-content-center main-button ${productPage ? 'inverted' : null}`}
+        style={{ height: productPage ? '100%' : null }}
+    >
+          В корзину&nbsp;<TbShoppingCartPlus />
+    </button>
+      : <button
+        onClick={() => {
+          window.scrollTo(0, 0)
+          navigate(LOGIN_ROUTE)
+        }}
+        className={`d-flex align-items-center justify-content-center main-button ${productPage ? 'inverted' : null} disabled`}
+        style={{ height: productPage ? '100%' : null }}
+        type='button'
+    >
+          В корзину&nbsp;<TbShoppingCartPlus />
+    </button>
   )
 }
 
